ea1acf583499b3e764e6de8647dce0326f2f64fd
DannyKu0357 <danil.kurilenko@icloud.com>
2022-11-21 12:11:00 +0800

    Complete practicle

--- lib/screens/AuthScreen.dart
+++ lib/screens/AuthScreen.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
-
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:rent_application/helpers/size_config.dart';
 import 'package:rent_application/theme/model_theme.dart';
 import 'package:provider/provider.dart';
 import 'package:pinput/pin_put/pin_put.dart';
+
+import '../helpers/helpers.dart';
+import '../helpers/message_exception.dart';
+import '../repository/firebase_auth.dart';
+import '../widgets/custom_snackBar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -18,6 +24,15 @@ class _AuthScreenState extends State<AuthScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final GlobalKey<FormState> _formKeyRegister = GlobalKey<FormState>();
   int numScreen = 1;
+  String _email = '';
+  String _password = '';
+  bool _obscurePassword = true;
+  String _verificationID = '';
+  bool codeSumbit = false;
+  bool codeVerify = true;
+  final _pinPutController = TextEditingController();
+
+  TextEditingController _phone = TextEditingController();
   getPage() {
     switch (numScreen) {
       case 1:
@@ -34,6 +49,58 @@ class _AuthScreenState extends State<AuthScreen> {
     setState(() {
       numScreen = value;
     });
+  }
+
+  void _validateAuth() async {
+    final FormState? form = _formKey.currentState;
+    if (_formKey.currentState!.validate()) {
+      helpers.showProgress(
+          context, 'Выполняется вход, пожалуйста подождите', false);
+      form?.save();
+      try {
+        bool result = await fbAuth.auth(_email, _password);
+        if (result) {
+          helpers.hideProgress();
+          print('Document exists on the database');
+          Navigator.pushNamedAndRemoveUntil(
+              context, 'tabNavigator', (Route<dynamic> route) => false);
+        } else {
+          helpers.hideProgress();
+          Navigator.pushNamed(context, 'registrationScreen');
+        }
+      } on MessageException catch (e) {
+        helpers.hideProgress();
+        print(e);
+        CustomSnackBar(context, Text(e.message), Colors.lightGreen);
+      }
+    }
+  }
+
+  _phoneAuth() async {
+    try {
+      await fbAuth.submitPhoneNumber(
+          phoneNumber: _phone.text,
+          func: (value) {
+            setState(() {
+              _verificationID = value;
+            });
+            if (_verificationID != null) {
+              CustomSnackBar(
+                  context, Text('СМС код был выслан'), Colors.lightGreen);
+              setState(() {
+                codeSumbit = false;
+                numScreen = 4;
+              });
+            }
+          },
+          durationCode: () {
+            setState(() {
+              codeSumbit = true;
+            });
+          });
+    } on MessageException catch (e) {
+      CustomSnackBar(context, Text(e.message), Colors.red);
+    }
   }
 
   @override
@@ -80,15 +147,6 @@ class _AuthScreenState extends State<AuthScreen> {
                         child: Column(
                           children: [
                             Container(
-                              height: 50.0.toAdaptive(context),
-                              child: Center(
-                                  child:
-                                      Image.asset("assets/img/logo_text.png")),
-                            ),
-                            Expanded(
-                              child: Container(),
-                            ),
-                            Container(
                               padding: EdgeInsets.symmetric(
                                   horizontal: 10, vertical: 15),
                               width: MediaQuery.of(context).size.width,
@@ -119,7 +177,26 @@ class _AuthScreenState extends State<AuthScreen> {
                                             MediaQuery.of(context).size.width /
                                                 10),
                                     child: PinPut(
-                                        onSubmit: (value) async {},
+                                        onSubmit: (value) async {
+                                          try {
+                                            bool value =
+                                                await fbAuth.submitCode(
+                                                    code:
+                                                        _pinPutController.text,
+                                                    verificationId:
+                                                        _verificationID,
+                                                    context: context);
+                                            if (!value) {
+                                              setState(() {
+                                                codeVerify = false;
+                                              });
+                                            }
+                                          } on MessageException catch (e) {
+                                            CustomSnackBar(context,
+                                                Text(e.message), Colors.red);
+                                          }
+                                        },
+                                        controller: _pinPutController,
                                         fieldsCount: 6,
                                         fieldsAlignment:
                                             MainAxisAlignment.spaceAround,
@@ -134,6 +211,9 @@ class _AuthScreenState extends State<AuthScreen> {
                                               Color.fromRGBO(197, 206, 224, 1),
                                         )),
                                   ),
+                                  if (!codeVerify)
+                                    Text('Неверный код',
+                                        style: TextStyle(fontSize: 14)),
                                 ],
                               ),
                             ),
@@ -169,29 +249,6 @@ class _AuthScreenState extends State<AuthScreen> {
               'Вход по Email',
               style: TextStyle(fontSize: 14),
             ),
-            InkWell(
-              onTap: () {
-                setState(() {
-                  numScreen = 3;
-                });
-              },
-              child: Row(
-                children: [
-                  Text(
-                    'Регистрация',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  // SvgPicture.asset(
-                  //   'assets/svg/arrow.svg',
-                  //   width: 6,
-                  //   height: 10,
-                  // )
-                ],
-              ),
-            ),
           ],
         ),
         SizedBox(
@@ -202,7 +259,7 @@ class _AuthScreenState extends State<AuthScreen> {
           padding: EdgeInsets.only(top: 25, left: 10, right: 10, bottom: 20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(5)),
-            color: Colors.white,
+            color: Theme.of(context).cardColor,
           ),
           child: Form(
             key: _formKey,
@@ -217,14 +274,30 @@ class _AuthScreenState extends State<AuthScreen> {
               ),
               TextFormField(
                 onSaved: (input) {
-                  //_email = input;
+                  _email = input!;
                 },
-                //validator: emailValidator,
+                validator: (value) {
+                  if (value != null || value!.isNotEmpty) {
+                    final RegExp regex = RegExp(
+                        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)| (\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+                    if (!regex.hasMatch(value))
+                      return 'Введите корректный email';
+                    else
+                      return null;
+                  } else {
+                    return 'Введите корректный email';
+                  }
+                },
                 decoration: InputDecoration(
-                  //errorStyle: validateText,
+                  errorStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFFFF0000),
+                  ),
+
                   contentPadding:
                       EdgeInsets.symmetric(vertical: 15, horizontal: 0),
-                  // prefixIcon: Container(
+                  prefixIcon: Container(child: Icon(Icons.email)),
                   //   transform: Matrix4.translationValues(-10.0, 0.0, 0.0),
                   //   child: SvgPicture.asset(
                   //     'assets/svg/mail.svg',
@@ -254,40 +327,36 @@ class _AuthScreenState extends State<AuthScreen> {
                 height: 15,
               ),
               TextFormField(
-                //obscureText: _obscurePassword,
-                //onSaved: (input) => _password = input,
-                // validator: (input) {
-                //   if (input.isEmpty) {
-                //     return "Неверный пароль";
-                //   } else {
-                //     if (input.length < 6) {
-                //       return "Пароль слишком короткий";
-                //     } else {
-                //       return null;
-                //     }
-                //   }
-                // },
+                obscureText: _obscurePassword,
+                onSaved: (input) => _password = input!,
+                validator: (input) {
+                  if (input!.isEmpty) {
+                    return "Неверный пароль";
+                  } else {
+                    if (input.length < 6) {
+                      return "Пароль слишком короткий";
+                    } else {
+                      return null;
+                    }
+                  }
+                },
                 decoration: InputDecoration(
-                  //errorStyle: validateText,
+                  errorStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFFFF0000)),
                   contentPadding:
                       EdgeInsets.symmetric(vertical: 15, horizontal: 0),
-                  // suffixIcon: InkWell(
-                  //     onTap: () {
-                  //       // setState(() {
-                  //       //   _obscurePassword = !_obscurePassword;
-                  //       // });
-                  //     },
-                  //     child: SvgPicture.asset('assets/svg/eye.svg',
-                  //         width: 15, height: 15, fit: BoxFit.scaleDown)),
-                  // prefixIcon: Container(
-                  //   transform: Matrix4.translationValues(-10.0, 0.0, 0.0),
-                  //   child: SvgPicture.asset(
-                  //     'assets/svg/key.svg',
-                  //     fit: BoxFit.scaleDown,
-                  //     width: 15,
-                  //     height: 12,
-                  //   ),
-                  // ),
+                  suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                      child: Icon(Icons.remove_red_eye)),
+                  prefixIcon: Container(
+                    child: Icon(Icons.password),
+                  ),
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(color: Color(0xFFC5CEE0)),
                   ),
@@ -305,7 +374,43 @@ class _AuthScreenState extends State<AuthScreen> {
               SizedBox(
                 height: 25.0,
               ),
-              //PrimaryButton(onPressed: _validateAuth, text: 'Войти'),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 20)),
+                  onPressed: () {
+                    _validateAuth();
+                      _phoneAuth() async {
+                        try {
+                          await fbAuth.submitPhoneNumber(
+                            phoneNumber: _phone.text,
+                            func: (value) {
+                              setState(() {
+                                _verificationID = value;
+                              });
+                              if (_verificationID != null) {
+                                CustomSnackBar(context, Text('СМС код был выслан'), Colors.lightGreen);
+                                setState(() {
+                                  codeSumbit = false;
+                                  numScreen = 4;
+                                });
+                             }
+                           },
+                          durationCode: () {
+                            setState(() {
+                              codeSumbit = true;
+                             });
+                          });
+                      } on MessageException catch (e) {
+                        CustomSnackBar(context, Text(e.message), Colors.red);
+                      }
+                    }
+                  },
+                  child: const Text('Войти'),
+                ),
+              ),
+
               SizedBox(
                 height: 14,
               ),
@@ -343,8 +448,8 @@ class _AuthScreenState extends State<AuthScreen> {
   }
 
   Widget phoneForm() {
-    // var maskFormatter = new MaskTextInputFormatter(
-    //     mask: '+7 (###) ###-##-##', filter: {"#": RegExp(r'[0-9]')});
+    var maskFormatter = new MaskTextInputFormatter(
+        mask: '+7 (###) ###-##-##', filter: {"#": RegExp(r'[0-9]')});
     return Column(
       children: [
         Text('Добро пожаловать', style: TextStyle(fontSize: 14)),
@@ -356,7 +461,7 @@ class _AuthScreenState extends State<AuthScreen> {
           padding: EdgeInsets.only(top: 25, left: 10, right: 10, bottom: 20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(5)),
-            color: Colors.white,
+            color: Theme.of(context).cardColor,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -370,8 +475,8 @@ class _AuthScreenState extends State<AuthScreen> {
               ),
               TextField(
                 keyboardType: TextInputType.number,
-                //inputFormatters: [maskFormatter],
-                //controller: _phone,
+                inputFormatters: [maskFormatter],
+                controller: _phone,
                 decoration: InputDecoration(
                     enabledBorder: UnderlineInputBorder(
                         borderSide: BorderSide(color: Color(0xFFC5CEE0))),
@@ -381,20 +486,20 @@ class _AuthScreenState extends State<AuthScreen> {
               SizedBox(
                 height: 25,
               ),
-
               SizedBox(
                 height: 25,
               ),
-              // PrimaryButton(
-              //     onPressed: () {
-              //       try {
-              //         _phoneAuth();
-              //       } on MessageException catch (e) {
-              //         _scaffoldKey?.currentState
-              //             ?.showSnackBar(SnackBarScope.show(e.message));
-              //       }
-              //     },
-              //     text: 'Войти'),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 20)),
+                  onPressed: () {
+                    _phoneAuth();
+                  },
+                  child: const Text('Войти'),
+                ),
+              ),
               SizedBox(
                 height: 25,
               ),
